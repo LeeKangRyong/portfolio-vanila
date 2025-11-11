f540168db40cab3ae1c9154766b4670ba8242e17
LeeKangRyong <gaiogo2@naver.com>
2025-11-11 16:17:22 +0900

    feat: Add skill data and icons

---
+++ src/data/skillData.js
@@ -0,0 +1,58 @@
+const SKILL = [
+    {
+        TITLE: 'LANGUAGE',
+        NAME: [
+            'JavaScript', 'Python'
+        ],
+        ICON: [
+            'js', 'python'
+        ]
+    },
+    {
+        TITLE: 'FE',
+        NAME: [
+            'React', 'React\nNative', 'Zustand', 
+            'HTML', 'CSS'
+        ],
+        ICON: [
+            'react', 'react', 'zustand',
+            'html', 'css'
+        ]
+    },
+    {
+        TITLE: 'BE',
+        NAME: [
+            'Node.js', 'MySQL', 'Prisma'
+        ],
+        ICON: [
+            'nodejs', 'mysql', 'prisma'
+        ]
+    },
+    {
+        TITLE: 'DEPLOYMENT',
+        NAME: [
+            'Netlify, Verce, AWS, Azure, Github\nActions'
+        ],
+        ICON: [
+            'netlify', 'vercel', 'aws', 'azure', 'github'
+        ]
+    },
+    {
+        TITLE: 'COMMUNICATION',
+        NAME: [
+            'Notion', 'Slack', 'Github'
+        ],
+        ICON: [
+            'notion', 'slack', 'github'
+        ]
+    },
+    {
+        TITLE: 'ETC',
+        NAME: [
+            'Figma', 'Canva', 'ML/DL', 'Data\nAnalysis', 'Git'
+        ],
+        ICON: [
+            'figma', 'canva', 'ai', 'data', 'git'
+        ]
+    }
+]
