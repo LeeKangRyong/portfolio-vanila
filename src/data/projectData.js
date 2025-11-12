f39fb8eb14ea041df2e2bf061c09263d501216ed
LeeKangRyong <gaiogo2@naver.com>
2025-11-12 13:36:16 +0900

    Add project data, not assets

---
+++ src/data/projectData.js
@@ -0,0 +1,134 @@
+const PROJECT = [
+    {
+        TITLE: "포트폴리오",
+        THUMBNAIL: "portfolio_thumbnail",
+        TOP_COLOR: "#252c26",
+        DESCRIPTION: "개인 포트폴리오 웹사이트",
+        MEMBER: "개발 (1)",
+        ROLE: "기획, 디자인, 개발",
+        DURATION: "2025.06 ~ 2025.07 (2주)",
+        FE_STACK: [
+            "React", "Java\nScript", "HTML",
+            "CSS", "Framer Motion"
+        ],
+        BE_STACK: [
+            "X"
+        ],
+        DEPLOYMENT: [
+            "Netlify"
+        ],
+        TOOL: [
+            "Github", "Figma"
+        ],
+        GITHUB: "https://github.com/LeeKangRyong/portfolio",
+        IMAGES: [
+            "portfolio_1", "portfolio_2", "portfolio_3", "portfolio_4"
+        ],
+        ARCHITECTURE: "portfolio_architecture"
+    },
+    {
+        TITLE: "EncapMoments",
+        THUMBNAIL: "encap_thumbnail",
+        TOP_COLOR: "#aaab52",
+        DESCRIPTION: "AI 미션 기반 추억을 만들고 저장하는 가족 공유 앱서비스",
+        MEMBER: "BE (2), FE( (1)",
+        ROLE: "기획, 디자인, FE",
+        DURATION: "2025.03 ~ 2025.05",
+        FE_STACK: [
+            "React\nNative", "Java\nScript", "HTML",
+            "CSS", "Zustand"
+        ],
+        BE_STACK: [
+            "Node.js", "Java\nScript", "MySQL", "Prisma"
+        ],
+        DEPLOYMENT: [
+            "AWS", "NGINX", "Github\nActions"
+        ],
+        TOOL: [
+            "Github", "Figma", "Notion"
+        ],
+        GITHUB: "https://github.com/encapmoments",
+        IMAGES: [
+            "encap_1", "encap_2", "encap_3", "encap_4"
+        ],
+        ARCHITECTURE: "encap_architecture"
+    },
+    {
+        TITLE: "당신의 심장건강 비서, 콩콩",
+        THUMBNAIL: "kong_thumbnail",
+        TOP_COLOR: "#dc2626",
+        DESCRIPTION: "RAG 기반 AI Agent '콩콩이'를 탑재한 헬스케어 앱",
+        MEMBER: "AI (3), 디자인+기획 (1), 개발 (1)",
+        ROLE: "풀스택 개발",
+        DURATION: "2025.05 ~ 2025.08",
+        FE_STACK: [
+            "React\nNative", "Java\nScript", "HTML",
+            "Styled\nComponents", "Zustand"
+        ],
+        BE_STACK: [
+            "Node.js", "Java\nScript", "MySQL", "Prisma",
+            "FastAPI", "Python"
+        ],
+        DEPLOYMENT: [
+            "AWS", "NGINX", "Github\nActions", "Ngrok"
+        ],
+        TOOL: [
+            "Github", "Figma", "Notion"
+        ],
+        GITHUB: "https://github.com/Prometheus-AI-Project/2025_1_Heart_Docter_kongkong",
+        IMAGES: [
+            "kong_1", "kong_2", "kong_3", "kong_4"
+        ],
+        ARCHITECTURE: "kong_architecture"
+    },
+    {
+        TITLE: "포트폴리오 (VanliaJS)",
+        DESCRIPTION: "순수 JS로 재구현 및 리팩토링한 포트폴리오 웹사이트",
+        TOP_COLOR: "#dba231",
+        THUMBNAIL: "portfolio_vanila_thumbnail",
+        MEMBER: "개발 (1)",
+        ROLE: "기획, 디자인, 개발",
+        DURATION: "2025.11 ~ 2025.11 (1주)",
+        FE_STACK: [
+            "Java\nScript", "HTML", "CSS"
+        ],
+        BE_STACK: [
+            "X"
+        ],
+        DEPLOYMENT: [
+            "Netlify"
+        ],
+        TOOL: [
+            "Github", "Figma"
+        ],
+        GITHUB: "https://github.com/LeeKangRyong/portfolio-vanila",
+        IMAGES: [
+            "portfolio_vaila_1", "portfolio_vaila_2", "portfolio_vaila_3", "portfolio_vaila_4"
+        ],
+        ARCHITECTURE: "portfolio_vanila_architecture"
+    },
+    {
+        TITLE: "TeamWorks",
+        DESCRIPTION: "협업 시 반복적으로 발생하는 무임승차 문제를 데이터 기반으로 해결하는 공정 협업 관리 플랫폼",
+        TOP_COLOR: "#3666Fa",
+        THUMBNAIL: "teamworks_thumbnail",
+        MEMBER: "개발 (2), 디자인 (1), 기획 (1)",
+        ROLE: "FE 개발",
+        DURATION: "2025.08 ~ 현재",
+        FE_STACK: [
+            "Next.js", "Java\nScript", "HTML", "TailwindCSS",
+            "Zustand", "ReChart", "Chart.js", "Netlify"
+        ],
+        BE_STACK: [
+            "Kotlin", "Spring", "Redis", "PostgraSQL"
+        ],
+        DEPLOYMENT: [
+            "Netlify", "NCP"
+        ],
+        GITHUB: "https://github.com/LeeKangRyong/teamworks-fe",
+        IMAGES: [
+            "teamworks_1", "teakworks_2", "teamworks_3", "teamworks_4"
+        ],
+        ARCHITECTURE: "teamworks_architecture"
+    }
+]
