e5f75d67ee536e249679abcea85652f554babcdb
LeeKangRyong <gaiogo2@naver.com>
2025-11-12 14:08:25 +0900

    feat: Implement contact UI
    Write todo list about functions (not connected yet)

---
+++ src/scripts/pages/Contact.js
@@ -0,0 +1,13 @@
+class Contact {
+    constructor () {}
+
+    copyContactText(contactText) {
+        // TODO: 클릭하면 행당 텍스트 복사
+    }
+
+    linkToContact(contactImage) {
+        // TODO: _blank로 해당 링크 열기
+    }
+}
+
+export { Contact };
